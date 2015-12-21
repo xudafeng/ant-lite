1b8a8f516f51138d283561f39b782e44515a7d8f
xudafeng <xudafeng@126.com>
2015-12-22 00:30:51 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+/* ================================================================
+ * ant-lite by xdf(xudafeng[at]126.com)
+ *
+ * first created at : Tue Dec 22 2015 00:25:50 GMT+0800 (CST)
+ *
+ * ================================================================
+ * Copyright  xdf
+ *
+ * Licensed under the MIT License
+ * You may not use this file except in compliance with the License.
+ *
+ * ================================================================ */
+
+'use strict';
+
+module.exports = require('./lib/ant-lite');
---
+++ test/ant-lite.test.js
@@ -0,0 +1,20 @@
+/* ================================================================
+ * ant-lite by xdf(xudafeng[at]126.com)
+ *
+ * first created at : Tue Dec 22 2015 00:25:50 GMT+0800 (CST)
+ *
+ * ================================================================
+ * Copyright 2013 xdf
+ *
+ * Licensed under the MIT License
+ * You may not use this file except in compliance with the License.
+ *
+ * ================================================================ */
+
+'use strict';
+
+var ant-lite = require('..');
+
+describe('test', function() {
+
+});
